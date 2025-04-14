1c5c0d9f8982ff6272f18ac1d05f9e2bb5741458
Zhihang Huang <zhihanghuang1999@gmail.com>
2025-04-14 19:00:45 +1000

    1

--- awareness_backend/settings.py
+++ awareness_backend/settings.py
@@ -95,9 +95,7 @@ pymysql.install_as_MySQLdb()
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
